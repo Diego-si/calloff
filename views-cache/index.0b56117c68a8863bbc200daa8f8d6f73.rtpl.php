17bd68ac7b1845df33d040cabdc70a1e9e06c369
Diego-si <diventura1@gmail.com>
2020-09-28 13:42:07 -0300

    tpl site

--- views-cache/index.0b56117c68a8863bbc200daa8f8d6f73.rtpl.php
+++ views-cache/index.0b56117c68a8863bbc200daa8f8d6f73.rtpl.php
@@ -1,1 +1,56 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>HELoo</h1>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="gif">
+  <img src="https://cdn.discordapp.com/attachments/642859069257941002/743896937685712926/20200814_150914.gif" alt="Chicago" style="width:100%;">
+</div>
+
+<div class="container">
+  <div class="div" style="border: none;">
+      <iframe title="video" src="https://www.youtube.com/embed/ODIhhFU7Eug?autoplay=1&loop=1&autopause=0" width="100%" height="300px" style="border: none;"></iframe></iframe>
+  </div>
+</div>
+
+<div class="container">
+  <h2>APOIADORES</h2>
+  <!-- <div class="row">
+    <div class="col-md-6">
+      <div class="item active">
+        <img src="https://cdn.discordapp.com/attachments/642859069257941002/717170548526022686/easy.jpeg" alt="Los Angeles" style="width:100%;">
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="item">
+        <img src="https://cdn.discordapp.com/attachments/642859069257941002/717170551734534175/picgtk.jpeg" alt="Chicago" style="width:100%;">
+      </div>
+    </div>
+  </div> -->
+  <div class="slider">
+    <figure>
+      <img src='https://cdn.discordapp.com/attachments/642859069257941002/744178003545817198/1592665075432.png'>
+      <img src='https://cdn.discordapp.com/attachments/642859069257941002/744525512722087946/20200815_174247.png'>
+      <img src='https://cdn.discordapp.com/attachments/642859069257941002/744178010151845918/1592698487109.png'>
+      <img src='https://cdn.discordapp.com/attachments/642859069257941002/744178055421100072/LOGO_ORION.png'>
+      <img src='https://cdn.discordapp.com/attachments/642859069257941002/744178082654715994/royalty_gaming_sem_fundo.png'>
+      <img src='https://cdn.discordapp.com/attachments/642859069257941002/744178094642036856/Logo_Renove_Team.png'>
+      <img src='https://cdn.discordapp.com/attachments/642859069257941002/744178103768580136/1593049537025.png'>
+    </figure>
+  </div>
+</div>
+
+<div class="container-fluid">
+  <div class="row justify-content-md-center" style="margin: 5px;">
+    <div class="col-md-6">
+      <p>Ao participar dos principais campeonatos de Call Of Duty Mobile na América do Sul, assim como em fases Mundiais, os times são classificados no Ranking geral de acordo com seu desempenho nas partidas.</p>
+    </div>
+    <div class="col-md-8">
+      <p>Logo, quanto melhor for o trabalho em equipe e planejamento de estratégias, maiores serão as chances de que um time alcance o topo.</p>
+    </div>
+    <div class="col-md-8">
+      <p>De maneira geral, o Ranking S.A de CODM é utilizado para tornar mais competitivo ,e incentivar mais as equipes a ir a batalha ,além de registrar oficialmente em nosso site quais são os melhores times.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="div" style="border: none;">
+      <iframe title="video" src="https://www.youtube.com/embed/vTU6f7mCWLE" width="100%" height="300px" style="border: none;"></iframe></iframe>
+  </div>
+</div>
